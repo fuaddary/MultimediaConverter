1406c53388a6728ee25fa59463baf7dde9096c80
fuaddary <fuaddary@gmail.com>
2018-10-05 16:23:16 +0700

    new template

---
+++ resources/views/components/css.blade.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" id="css-main" href="assets/css/codebase.min.css">
+@yield('custom-css')
---
+++ resources/views/components/js.blade.php
@@ -0,0 +1,10 @@
+<script src="assets/js/core/jquery.min.js"></script>
+<script src="assets/js/core/bootstrap.bundle.min.js"></script>
+<script src="assets/js/core/jquery.slimscroll.min.js"></script>
+<script src="assets/js/core/jquery.scrollLock.min.js"></script>
+<script src="assets/js/core/jquery.appear.min.js"></script>
+<script src="assets/js/core/jquery.countTo.min.js"></script>
+<script src="assets/js/core/js.cookie.min.js"></script>
+<script src="assets/js/codebase.js"></script>
+
+@yield('custom-js')
